ad1c383179be7f92fdbca0e5e03d84422e0bff9d
Banana21-us <60498922+Banana21-us@users.noreply.github.com>
2026-09-15 23:59:06 +0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
            this is Mario and Miko and Glen test on development
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-           yoyoyoyoyoyoyo{" "}
+           yoyoyoyoyoyoyoy{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
